28bd725e0b42bb92c4df0b95447280cfe5ae212f
Diego Rocha Serejo <diego.serejo@tce.ma.gov.br>
2026-02-09 09:09:45 -0300

    Livros

--- bookhub/src/main/java/com/bookhub/entities/Amigo.java
+++ bookhub/src/main/java/com/bookhub/entities/Amigo.java
@@ -22,42 +22,34 @@ public class Amigo {
     public Amigo() {}
 
     public Long getId() {
-
         return id;
     }
 
     public void setId(Long id) {
-
         this.id = id;
     }
 
     public String getName() {
-
         return name;
     }
 
     public void setName(String name) {
-
         this.name = name;
     }
 
     public String getEmail() {
-
         return email;
     }
 
     public void setEmail(String email) {
-
         this.email = email;
     }
 
     public String getPhone() {
-
         return phone;
     }
 
     public void setPhone(String phone) {
-
         this.phone = phone;
     }
 
@@ -66,7 +58,6 @@ public class Amigo {
     }
 
     public void setLoans(Set<Loan> loans) {
-
         this.loans = loans;
     }
 }
